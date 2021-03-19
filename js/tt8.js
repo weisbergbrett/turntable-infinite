650ee01616560342bc0ba8415fd13617c78468fa
Brett Weisberg <weisbergbrett96@gmail.com>
2021-03-19 11:38:04 -0700

    Update tt8.js

--- js/tt8.js
+++ js/tt8.js
@@ -1,3 +1,10 @@
 /* Copyright (c) 2021 Brett Weisberg */
-//$('head').append('<link rel="stylesheet" type="text/css" href="style.css">');
+
+//MAIN
+/*$('head').append('<link rel="stylesheet" type="text/css" href="style.css">');*/
+
+//CONSOLE
 $('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/weisbergbrett/turntable-infinite@main/css/style.css">');
+
+//BOOKMARK
+/*javascript:(function(){ $('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/weisbergbrett/turntable-infinite@main/css/style.css">'); })();*/
